c73ce63b19d4e9286c57fe24b8fdfed8942d0039
divyanshi2906 <divyanshidixit425@gmail.com>
2025-04-13 13:43:22 +0530

    add ResumeService interface with methods for Resume creation

---
+++ src/main/java/io/github/premwadhwani24/resumeanalyzer/resumeanalyzer/Resume/ResumeService.java
@@ -0,0 +1,19 @@
+package io.github.premwadhwani24.resumeanalyzer.resumeanalyzer.Resume;
+
+import java.util.List;
+public interface ResumeService {
+
+    Resume createResume(Resume resume);
+
+    List<Resume> getAllResume();
+    Resume getResumeByName(String name);
+
+    Resume getResumeByEmail(String email);
+
+    Resume getResumeBySkills(String skills);
+
+    Resume getResumeByEducation(String eduction);
+
+    Resume getResumeByExperience(String experience);
+}
+
